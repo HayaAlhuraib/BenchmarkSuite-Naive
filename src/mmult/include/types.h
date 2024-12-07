426508ffa472feffe10c70854b4b54c1c880da79
Haya Alhuraib <hayawi8002@gmail.com>
2024-12-07 14:59:13 +0300

    Update types.h

--- src/mmult/include/types.h
+++ src/mmult/include/types.h
@@ -5,11 +5,14 @@
 
 // Define the argument structure
 typedef struct {
-    void* input;       // Pointer to the input data (e.g., matrices A and B as floats)
-    void* output;      // Pointer to the output data (e.g., result matrix R as floats)
-    size_t size;       // Dimension size (used to define M, N, P)
-    int cpu;           // CPU core to execute the benchmark (optional)
-    int nthreads;      // Number of threads to use (optional for parallel implementation)
+  byte*   input;
+  byte*   output;
+
+  size_t size;
+
+  int     cpu;
+  int     nthreads;
+
 } args_t;
 
 #endif // __INCLUDE_TYPES_H_
